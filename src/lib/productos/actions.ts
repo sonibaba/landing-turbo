1988b76f9c6c7b5e6b93c694d3a1a67b8af62c4a
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-28 16:33:47 -0600

    feat (productos actions): Configurando fetch

--- src/lib/productos/actions.ts
+++ src/lib/productos/actions.ts
@@ -1,23 +1,33 @@
 import { IProducto, IProductos } from '@/model/productos'
 import fetchResponse from '../fetch'
 
-const getProductos = async (): Promise<IProducto> => {
-  const response = await fetchResponse('productos?estatus=true', 'GET')
-  if (!response) {
+const getProductos = async (): Promise<{
+  error: boolean
+  message?: string
+  data: IProducto
+}> => {
+  const { error, data, message } = await fetchResponse('productos?estatus=true', 'GET')
+
+  if (error) {
     return {
-      combos: [],
-      complementos: [],
-      pizzas: [],
+      error,
+      message,
+      data: { combos: [], complementos: [], pizzas: [] },
     }
   }
-  const obj = Object.groupBy(response, (producto: IProductos) => producto.categoria)
 
-  console.log(obj)
+  const obj = Object.groupBy(
+    data as unknown as Array<IProductos>,
+    (producto: IProductos) => producto.categoria
+  )
 
   return {
-    combos: obj.combos ?? [],
-    complementos: obj.complementos ?? [],
-    pizzas: obj.pizzas ?? [],
+    error,
+    data: {
+      combos: obj.combos ?? [],
+      complementos: obj.complementos ?? [],
+      pizzas: obj.pizzas ?? [],
+    },
   }
 }
 
