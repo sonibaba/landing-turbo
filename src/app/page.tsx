702b1f8597918487e530b46083c43f3976cfebf4
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-28 16:32:34 -0600

    feat (app page): Agregando navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
+'use client'
+
 import Banner from '@/components/home/Banner'
 import Footer from '@/components/home/Footer'
 import HeroPedido from '@/components/home/HeroPedido'
 import Navbar from '@/components/home/Navbar'
+import NavbarMobile from '@/components/home/NavbarMobile'
 import Productos from '@/components/home/Productos'
+import { useAdvancedDevice } from '@/hooks/isMobile'
 
 export default function Home() {
+  const isMobile = useAdvancedDevice().isMobile
+
   return (
     <div>
       <main>
-        <Navbar />
+        {isMobile ? <NavbarMobile /> : <Navbar />}
         <Banner />
         <HeroPedido />
         <Productos />
