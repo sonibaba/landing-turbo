3ae8068a9b7154bf4ba943ea4fab2be891620c27
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-28 16:33:26 -0600

    feat (fetch): Configurando fetch

--- src/lib/fetch.ts
+++ src/lib/fetch.ts
@@ -2,7 +2,11 @@ interface ArgsDynamic {
   [key: string]: unknown
 }
 
-const fetchResponse = async (url: string, method: string, args?: ArgsDynamic) => {
+const fetchResponse = async (
+  url: string,
+  method: string,
+  args?: ArgsDynamic
+): Promise<{ error: boolean; message?: string; data?: ArgsDynamic }> => {
   const response = await fetch(`https://sandbox3.huastecanetwork.com/api/${url}`, {
     method,
     headers: {
@@ -13,10 +17,16 @@ const fetchResponse = async (url: string, method: string, args?: ArgsDynamic) =>
   })
   if (response.status != 200) {
     console.log(response)
-    return null
+    return {
+      error: true,
+      message: await response.text(),
+    }
   }
   const data = await response.json()
-  return data
+  return {
+    error: false,
+    data: data,
+  }
 }
 
 export default fetchResponse
