1012a5f2b8cadaf0724f28cbb38cf1cc7b30efb3
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-28 16:30:12 -0600

    feat (next config): Cambiando comportamiento de image

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,13 @@ const nextConfig: NextConfig = {
     CLOUDINARY_CLOUD_NAME: 'dt5bkke6v',
     CLOUDINARY_URL: 'cloudinary://API_KEY:API_SECRET@dt5bkke6v',
   },
+  distDir: 'out',
   // i18n: {
   //   locales: ['es-MX'],
   //   defaultLocale: 'es-MX',
   // },
   images: {
+    unoptimized: true,
     localPatterns: [
       {
         pathname: './src/assets/*',
