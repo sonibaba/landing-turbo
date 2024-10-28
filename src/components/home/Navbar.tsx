7e2b57f33994c39af29a252faeac0ea4beafe850
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-28 16:37:36 -0600

    feat (navbar): Nuevos ajustes

--- src/components/home/Navbar.tsx
+++ src/components/home/Navbar.tsx
@@ -30,8 +30,8 @@ const Navbar = () => {
   }, [])
 
   return (
-    <nav className="bg-white shadow-lg">
-      <div className="max-w-7xl mx-auto">
+    <nav className="bg-white">
+      <div className="max-w-full mx-4">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <Link href="/" className="flex items-center">
@@ -86,8 +86,7 @@ const Navbar = () => {
                 <button onClick={() => setOpenBag(!openBag)}>
                   <FontAwesomeIcon
                     icon={faBagShopping}
-                    className="text-primary px-3 py-2 rounded-md text-sm font-medium transition duration-300 ease-in-out flex items-center cursor-pointer"
-                    size="xl"
+                    className="text-primary px-3 py-2 rounded-md text-sm font-medium transition duration-300 ease-in-out flex items-center cursor-pointer h-6 w-6"
                   />
                 </button>
                 {openBag && <Carrito />}
