c97658a7deec8645588b0a0308cfc40a835cf893
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-29 13:27:37 -0600

    feat (productos actions): Moviendo fetch a actions dictado por NextJS

---
+++ src/app/actions/productos.tsx
@@ -0,0 +1,34 @@
+import fetchResponse from '@/app/actions/fetch'
+import { IProducto, IProductos } from '@/model/productos'
+
+const getProductos = async (): Promise<{
+  error: boolean
+  message?: string
+  data: IProducto
+}> => {
+  const { error, data, message } = await fetchResponse('productos?estatus=true', 'GET')
+
+  if (error) {
+    return {
+      error,
+      message,
+      data: { combos: [], complementos: [], pizzas: [] },
+    }
+  }
+
+  const obj = Object.groupBy(
+    data as unknown as Array<IProductos>,
+    (producto: IProductos) => producto.categoria
+  )
+
+  return {
+    error,
+    data: {
+      combos: obj.combos ?? [],
+      complementos: obj.complementos ?? [],
+      pizzas: obj.pizzas ?? [],
+    },
+  }
+}
+
+export { getProductos }
