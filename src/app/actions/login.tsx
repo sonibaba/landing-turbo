e2be0e606a788415ded2140c92141da76178b122
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-29 13:27:12 -0600

    feat (login action): Validando login antes de iniciar session

---
+++ src/app/actions/login.tsx
@@ -0,0 +1,124 @@
+import fetchResponse from '@/app/actions/fetch'
+import {
+  SigninFormState,
+  SignupFormSchema,
+  SignupFormState,
+  SinginFormSchema,
+} from '@/lib/definitions'
+import { redirect } from 'next/navigation'
+
+const login = async (
+  prevState: SignupFormState | undefined,
+  formData: FormData
+): Promise<SignupFormState> => {
+  const validatedFields = SignupFormSchema.safeParse({
+    usuario: formData.get('email'),
+    password: formData.get('password'),
+  })
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      success: false,
+    }
+  }
+
+  try {
+    const response = await fetchResponse('auth', 'POST', {
+      usuario: formData.get('email'),
+      flag: 'cliente',
+      password: formData.get('password'),
+    })
+
+    if (!response) {
+      return {
+        errors: {
+          response: 'Error de conexión',
+        },
+        success: false,
+      }
+    }
+
+    const { error, message, data } = response
+
+    if (error) {
+      return {
+        errors: {
+          response: message,
+        },
+        success: false,
+      }
+    }
+
+    const access = data?.access_token as string
+    if (!access) {
+      return {
+        errors: {
+          response: 'Token de acceso no encontrado',
+        },
+        success: false,
+      }
+    }
+
+    localStorage.setItem('token', access)
+    redirect('/dashboard')
+  } catch (error) {
+    return {
+      errors: {
+        response: error instanceof Error ? error.message : 'Error inesperado',
+      },
+      success: false,
+    }
+  }
+}
+
+const registerClient = async (
+  state: SigninFormState | undefined,
+  { get }: FormData
+): Promise<{
+  errors?: {
+    nombre?: string[]
+    apePaterno?: string[]
+    apeMaterno?: string[]
+    email?: string[]
+    telefono?: string[]
+    direccion?: string[]
+    password?: string[]
+    response?: string
+  }
+  success?: boolean
+}> => {
+  const validatedFields = SinginFormSchema.safeParse({
+    usuario: get('email'),
+    nombre: get('nombre'),
+    apePaterno: get('apePaterno'),
+    apeMaterno: get('apeMaterno'),
+    direccion: get('direccion'),
+    email: get('email'),
+    telefono: get('telefono'),
+    password: get('password'),
+  })
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    }
+  }
+
+  const response = await fetchResponse('clientes', 'POST', {
+    usuario: get('email'),
+    password: get('password'),
+  })
+  if (!response) {
+    return {
+      errors: {
+        response: 'Ocurrio un error al crear el cliente',
+      },
+    }
+  }
+  return {
+    success: true,
+  }
+}
+
+export { login, registerClient }
