9cb6cb9c71ff2fcc12867a508310b41438eb89db
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-31 13:00:51 -0600

    feat (app page): Hacer pedido

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Footer from '@/components/home/Footer'
 import HeroPedido from '@/components/home/HeroPedido'
 import Navbar from '@/components/home/Navbar'
 import NavbarMobile from '@/components/home/NavbarMobile'
+import PlaceOrder from '@/components/home/PlaceOrder'
 import Productos from '@/components/home/Productos'
 import { useAdvancedDevice } from '@/hooks/isMobile'
 
@@ -15,6 +16,7 @@ export default function Home() {
     <div>
       <main>
         {isMobile ? <NavbarMobile /> : <Navbar />}
+        <PlaceOrder />
         <Banner />
         <HeroPedido />
         <Productos />
