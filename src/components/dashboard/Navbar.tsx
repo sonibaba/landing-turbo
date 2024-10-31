479d714d76d2130d34bf4d6db65fcd8b0fb105c2
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-31 15:13:42 -0600

    feat (navbar): Configurando navbar mejora

--- src/components/dashboard/Navbar.tsx
+++ src/components/dashboard/Navbar.tsx
@@ -1,15 +1,28 @@
 'use client'
 
-import { faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+import useStore from '@/hooks/useStore'
+import useCartStore from '@/store/useCartStore'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { LogOut, ShoppingCart, User } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { redirect } from 'next/navigation'
+import { Button } from '../ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '../ui/dropdown-menu'
 
 const Navbar = () => {
+  const cantidad = useStore(useCartStore, state => state.productos)
+
   return (
     <nav className="bg-white border-b">
-      <div className="max-w-7xl mx-auto">
+      <div className="w-12/12 mx-2">
         <div className="flex justify-between h-16">
           <div className="flex-shrink-0 flex items-center">
             <Link href="/dashboard" className="flex items-center">
@@ -22,15 +35,46 @@ const Navbar = () => {
               />
             </Link>
           </div>
-          <div className="flex items-center">
-            <FontAwesomeIcon
+          <div className="flex-shrink-0 flex items-center">
+            <Link href="/dashboard/carrito">
+              <div className="relative inline-flex">
+                <ShoppingCart className="h-8 w-auto text-primary pr-2" />
+                <div className="absolute -top-2 -right-1 bg-primary text-white text-xs font-bold rounded-full h-5 w-5 flex items-center justify-center">
+                  {cantidad?.length ?? 0}
+                </div>
+              </div>
+            </Link>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost">
+                  Hola, Jorge
+                  <FontAwesomeIcon icon={faChevronDown} />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent>
+                <DropdownMenuLabel>Mi cuenta</DropdownMenuLabel>
+                <DropdownMenuItem>
+                  <User />
+                  <span>Perfil</span>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>
+                  <Link href="/logout">
+                    <LogOut />
+                    <span>Cerrar sesión</span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+
+            {/* <FontAwesomeIcon
               icon={faSignOutAlt}
               onClick={() => {
                 localStorage.clear()
                 redirect('/')
               }}
               className="text-primary  rounded-md text-sm font-medium transition duration-300 ease-in-out flex items-center cursor-pointer h-6 w-6"
-            />
+            /> */}
           </div>
         </div>
       </div>
