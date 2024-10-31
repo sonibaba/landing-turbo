130a5fec6b11996ad0362b13b93622a4fa887294
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-10-31 12:54:55 -0600

    feat (loading): Pantalla de carga

--- src/components/shared/loading.tsx
+++ src/components/shared/loading.tsx
@@ -8,12 +8,14 @@ interface SpinnerProps {
 
 export default function Spinner({ color = 'text-primary' }: SpinnerProps) {
   return (
-    <div className="w-lvw h-lvh absolute top-0 z-20 flex items-center justify-center">
-      <div className={`flex justify-center items-center absolute z-30`} role="status">
-        <LoaderCircle className={`animate-spin h-16 w-16 ${color}`} aria-hidden="true" />
-        <span className="sr-only text-black">Cargando...</span>
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-gray-600 bg-opacity-40">
+      <div className="flex flex-col items-center justify-center p-4 text-black ">
+        <LoaderCircle
+          className={`animate-spin h-12 w-12 sm:h-16 sm:w-16 ${color}`}
+          aria-hidden="true"
+        />
+        <span className="mt-2 text-sm sm:text-base text-gray-700">Cargando...</span>
       </div>
-      <div className="w-full h-full bg-gray-600 opacity-40"></div>
     </div>
   )
 }
