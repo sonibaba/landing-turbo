995b3b71757f282268262a0bc6694cadd62805f2
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-04 17:04:22 -0600

    feat (use card store): Computando informacion de tienda

--- src/store/useCartStore.ts
+++ src/store/useCartStore.ts
@@ -1,21 +1,59 @@
-import { IProductos } from '@/model/productos'
+import { Categoria, IProductos } from '@/model/productos'
 import { create } from 'zustand'
+import { createComputed } from 'zustand-computed'
+import { persist } from 'zustand/middleware'
 
-type State = {
+type Store = {
   productos: Array<IProductos>
-}
-
-type Actions = {
   addProducto: (producto: IProductos) => void
+  removeProducto: (index: number) => void
+  removeAll: () => void
 }
 
-const useCartStore = create<State & Actions>(set => ({
-  productos: [],
-  addProducto(producto) {
-    set(state => ({
-      productos: [...state.productos, producto],
-    }))
-  },
-}))
+type ComputedStore = {
+  totalAmount: number
+  totalPuntos: number
+}
+
+const computed = createComputed(
+  (state: Store): ComputedStore => ({
+    totalAmount: state.productos.reduce((sum, item) => sum + item.precio, 0),
+    totalPuntos: state.productos.reduce((sum, item) => {
+      sum = sum + item.puntos
+      if (item.orilla && item.categoria == Categoria.Pizzas) {
+        sum = sum + 1
+      }
+      return sum
+    }, 0),
+  })
+)
+
+const useCartStore = create<Store>()(
+  computed(
+    persist(
+      (set, get) => ({
+        productos: [],
+        addProducto(producto) {
+          set(() => ({
+            productos: [...get().productos, producto],
+          }))
+        },
+        removeProducto(index: number) {
+          set(() => ({
+            productos: get().productos.splice(index, 1),
+          }))
+        },
+        removeAll() {
+          set(() => ({
+            productos: get().productos.splice(0, get().productos.length),
+          }))
+        },
+      }),
+      {
+        name: 'productos-storage',
+      }
+    )
+  )
+)
 
 export default useCartStore
