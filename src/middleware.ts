99008fea8b1a0ee55fdc3687e7cdf312f80217dc
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-06 12:44:35 -0600

    feat (middleware): Cambiando middleare por nextjs

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,58 +1,60 @@
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+export { auth as middleware } from '@/auth'
 
-export async function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname
-  const token = request.cookies.get('token')?.value
+// import type { NextRequest } from 'next/server'
+// import { NextResponse } from 'next/server'
 
-  // Permitir acceso a la ruta raíz sin redirección
-  if (path === '/') {
-    return NextResponse.next()
-  }
+// export async function middleware(request: NextRequest) {
+//   const path = request.nextUrl.pathname
+//   const token = request.cookies.get('token')?.value
 
-  // Solo verificar autenticación para /dashboard
-  if (path.startsWith('/dashboard')) {
-    if (!token) {
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
+//   // Permitir acceso a la ruta raíz sin redirección
+//   if (path === '/') {
+//     return NextResponse.next()
+//   }
 
-    try {
-      // Aquí deberías verificar el token
-      // Por ejemplo, podrías hacer una petición a tu API para validar el token
-      const response = await fetch('https://sandbox3.huastecanetwork.com/api/auth', {
-        method: 'GET',
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
+//   // Solo verificar autenticación para /dashboard
+//   if (path.startsWith('/dashboard')) {
+//     if (!token) {
+//       return NextResponse.redirect(new URL('/login', request.url))
+//     }
 
-      if (response.status === 401) {
-        // Si el token no es válido, redirigimos al login
-        return NextResponse.redirect(new URL('/login', request.url))
-      }
+//     try {
+//       // Aquí deberías verificar el token
+//       // Por ejemplo, podrías hacer una petición a tu API para validar el token
+//       const response = await fetch('https://sandbox3.huastecanetwork.com/api/auth', {
+//         method: 'GET',
+//         headers: {
+//           Authorization: `Bearer ${token}`,
+//         },
+//       })
 
-      // Si el token es válido, permitimos que la solicitud continúe
-      return NextResponse.next()
-    } catch (error) {
-      console.error('Error al validar el token:', error)
-      // En caso de error, también redirigimos al login
-      return NextResponse.redirect(new URL('/login', request.url))
-    }
-  }
+//       if (response.status === 401) {
+//         // Si el token no es válido, redirigimos al login
+//         return NextResponse.redirect(new URL('/login', request.url))
+//       }
 
-  // Para todas las demás rutas, permitimos el acceso sin verificación
-  return NextResponse.next()
-}
+//       // Si el token es válido, permitimos que la solicitud continúe
+//       return NextResponse.next()
+//     } catch (error) {
+//       console.error('Error al validar el token:', error)
+//       // En caso de error, también redirigimos al login
+//       return NextResponse.redirect(new URL('/login', request.url))
+//     }
+//   }
 
-export const config = {
-  matcher: [
-    /*
-     * Coincide con todas las rutas de solicitud excepto las que comienzan con:
-     * - api (rutas API)
-     * - _next/static (archivos estáticos)
-     * - _next/image (optimización de imágenes)
-     * - favicon.ico (icono del favicon)
-     */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-}
+//   // Para todas las demás rutas, permitimos el acceso sin verificación
+//   return NextResponse.next()
+// }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Coincide con todas las rutas de solicitud excepto las que comienzan con:
+//      * - api (rutas API)
+//      * - _next/static (archivos estáticos)
+//      * - _next/image (optimización de imágenes)
+//      * - favicon.ico (icono del favicon)
+//      */
+//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//   ],
+// }
