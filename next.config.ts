42a7126ca9719d9aa1f612643ad2fe38637c6abb
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-06 14:37:25 -0600

    feat (nextjs config): Configurando carpeta de salida

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const nextConfig: NextConfig = {
     CLOUDINARY_CLOUD_NAME: 'dt5bkke6v',
     CLOUDINARY_URL: 'cloudinary://API_KEY:API_SECRET@dt5bkke6v',
   },
+  distDir: 'out',
   // i18n: {
   //   locales: ['es-MX'],
   //   defaultLocale: 'es-MX',
