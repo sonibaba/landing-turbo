8a05818397af4c6d1d10b3505c6476ac91504413
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-06 15:14:37 -0600

    feat (auth): mas exclusiones

--- src/auth.ts
+++ src/auth.ts
@@ -7,7 +7,16 @@ import { ZodError } from 'zod'
 import pages from './config/pages'
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+  ],
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
