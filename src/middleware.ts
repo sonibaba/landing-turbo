146c31d0eb67aef661c73e4ac46e43b3f7fee1ff
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-07 11:18:43 -0600

    feat (middleware): reconfig middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,36 @@
+// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+
+// export default clerkMiddleware(async (auth, request) => {
+//   if (!isPublicRoute(request)) {
+//     await auth.protect()
+//   }
+// })
+
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-     */
-    '/((?!public|api|_next|favicon.ico|sitemap.xml|robots.txt).*)',
-    '/account/:path*',
-    '/dashboard/:path*',
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
   ],
 }
+
+// export const config = {
+//   matcher: [
+//     /*
+//      * Match all request paths except for the ones starting with:
+//      * - api (API routes)
+//      * - _next/static (static files)
+//      * - _next/image (image optimization files)
+//      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+//      */
+//     '/((?!public|api|_next|favicon.ico|sitemap.xml|robots.txt).*)',
+//     '/account/:path*',
+//     '/dashboard/:path*',
+//   ],
+// }
 
 export { auth as middleware } from '@/auth'
 
