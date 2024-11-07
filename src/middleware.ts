7ea40d51de07683d3336747130e187571517c6b2
Jorge de Jesus Espinoza Martinez <jdjezmp@gmail.com>
2024-11-06 18:41:01 -0600

    feat (middleware): probando regex |

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,7 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico, sitemap.xml, robots.txt (metadata files)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+    '/((?!api|auth/session|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
   ],
 }
 
